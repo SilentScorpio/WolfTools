c4ed382b76e9b5638100241ccd91a8249f828a2e
WolfCub <WolfCub@leileiyedemacbook-pro.local>
2016-02-02 15:47:33 +0800

    Initial Commit

---
+++ WolfTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WolfTools
+//
+//  Created by WolfCub on 16/2/2.
+//  Copyright © 2016年 WolfCub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WolfTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WolfTools
+//
+//  Created by WolfCub on 16/2/2.
+//  Copyright © 2016年 WolfCub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
